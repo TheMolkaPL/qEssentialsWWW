46c648d3997f9c0bb4f68c602670ea101ef95521
TheMolkaPL <themolkapl@gmail.com>
2015-04-12 14:16:12 +0200

    Ukryjmy linki do kontaktu oraz logowania jeżeli nie są one skończone. Odkopmy listę komend z zakładki Więcej

--- assets/navbar.php
+++ assets/navbar.php
@@ -10,17 +10,17 @@
 			<ul class="nav navbar-nav pull-right">
 				<li><a href="./index.php">Strona główna</a></li>
 				<li><a href="./update.php">Aktualizacje</a></li>
+				<li><a href="./cmdperm.php">Komendy i uprawnienia</a></li>
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Więcej <b class="caret"></b></a>
 					<ul class="dropdown-menu">
 						<li><a href="./api.php">Dokumentacja API</a></li>
 						<li><a href="./authors.php">Autorzy projektu</a></li>
-						<li><a href="./cmdperm.php">Komendy i uprawnienia</a></li>
 						<li><a href="./changelog.php">Lista zmian (changelog)</a></li>
 					</ul>
 				</li>
-				<li><a href="./contact.php">Kontakt</a></li>
-				<li class="active"><a class="btn" href="./signin.php">Zaloguj / zarejestruj</a></li>
+				<!-- <li><a href="./contact.php">Kontakt</a></li> -->
+				<!-- <li class="active"><a class="btn" href="./signin.php">Zaloguj / zarejestruj</a></li> -->
 			</ul>
 		</div><!--/.nav-collapse -->
 	</div>
